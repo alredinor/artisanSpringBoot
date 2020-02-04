4ab1ea3274c6c456b13f8551990b719c20d12777
alban <alban.delouvee@gmail.com>
2020-02-04 14:59:11 +0100

    alban

--- src/main/java/artisanSpringBoot/repositories/ServiceRepository.java
+++ src/main/java/artisanSpringBoot/repositories/ServiceRepository.java
@@ -3,10 +3,11 @@ package artisanSpringBoot.repositories;
 import java.util.List;
 import java.util.Optional;
 
-import org.hibernate.service.Service;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import artisanSpringBoot.model.Metier;
+import artisanSpringBoot.model.Service;
 
 public interface ServiceRepository extends JpaRepository<Service, Long> {
 	
