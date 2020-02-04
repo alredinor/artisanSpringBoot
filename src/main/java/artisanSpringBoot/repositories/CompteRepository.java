2c717a5a191b086b66d7feebd3d9535390efc734
alban <alban.delouvee@gmail.com>
2020-02-04 15:17:48 +0100

    alban

--- src/main/java/artisanSpringBoot/repositories/CompteRepository.java
+++ src/main/java/artisanSpringBoot/repositories/CompteRepository.java
@@ -4,12 +4,13 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import artisanSpringBoot.model.Adresse;
 import artisanSpringBoot.model.Compte;
 
 public interface CompteRepository extends JpaRepository<Compte, Long> {
 	
-	List<Compte> findByIdCompte();
-	List<Compte> findByEmail();
-	List<Compte> findByAdresse();
+	List<Compte> findByIdCompte(Long idCompte);
+	List<Compte> findByEmail(String mail);
+	List<Compte> findByAdresse(Adresse adresse);
 	
 }
