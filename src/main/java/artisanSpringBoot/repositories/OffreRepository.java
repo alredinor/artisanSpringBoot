96cfeb4e57d0c410b807c1bebe69d37eaa104b6d
Nico <touboulnicolas@gmail.com>
2020-02-18 15:11:21 +0100

    nico offre

---
+++ src/main/java/artisanSpringBoot/repositories/OffreRepository.java
@@ -0,0 +1,9 @@
+package artisanSpringBoot.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import artisanSpringBoot.model.Offre;
+
+public interface OffreRepository extends JpaRepository <Offre, Integer>{
+
+}
