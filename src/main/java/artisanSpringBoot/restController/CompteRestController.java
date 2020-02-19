93aca6d6146df5b9cec26c1de29447ed896de665
Nico <touboulnicolas@gmail.com>
2020-02-19 13:54:15 +0100

    nico

--- src/main/java/artisanSpringBoot/restController/CompteRestController.java
+++ src/main/java/artisanSpringBoot/restController/CompteRestController.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -30,6 +31,7 @@ import artisanSpringBoot.repositories.CompteRepository;
 
 @RestController
 @RequestMapping("/rest/compte")
+@CrossOrigin("*")
 public class CompteRestController {
 
 	@Autowired
